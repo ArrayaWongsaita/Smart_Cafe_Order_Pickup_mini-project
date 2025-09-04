85c5ffffc9693e514a20751fb18902b3a9ae40d2
ArrayaWongsaita <Wongsaita.a@gmail.com>
2025-09-04 20:02:10 +0700

    fix(env): remove default values for environment variables in schema

--- client-customer/src/shared/config/envConfig.ts
+++ client-customer/src/shared/config/envConfig.ts
@@ -1,11 +1,6 @@
 import envSchema from '@/shared/schema/env.schema';
 
-const env = {
-  NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
-  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-};
-
-const { success, data, error } = envSchema.safeParse(env);
+const { success, data, error } = envSchema.safeParse(process.env);
 
 if (!success) {
   console.error('Environment variables validation failed:', error);
--- client-customer/src/shared/schema/env.schema.ts
+++ client-customer/src/shared/schema/env.schema.ts
@@ -1,8 +1,8 @@
 import z from 'zod';
 
 const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.url().default('http://localhost:8000'),
-  NEXT_PUBLIC_SOCKET_URL: z.url().default('http://localhost:8000'),
+  NEXT_PUBLIC_API_BASE_URL: z.url(),
+  NEXT_PUBLIC_SOCKET_URL: z.url(),
   AUTH_SECRET: z.string().min(32).max(100).default('your_auth_secret'),
 });
 
